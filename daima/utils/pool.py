ef2ce32b0606e796d195f1074497a8d332fd1d22
heihudog <86613615+heihudog@users.noreply.github.com>
2025-03-03 17:24:09 +0800

    Add files via upload

---
+++ daima/utils/pool.py
@@ -0,0 +1,95 @@
+
+import torch
+from option import getargs
+args = getargs()
+
+
+
+
+
+class Percentile(torch.autograd.Function):
+    def __init__(self):
+        super().__init__()
+
+
+    def __call__(self, input, percentiles):
+        return self.forward(input, percentiles)
+
+
+    def forward(self, input, percentiles):
+        input = torch.flatten(input) # find percentiles for flattened axis
+        input_dtype = input.dtype
+        input_shape = input.shape
+        if isinstance(percentiles, int):
+            percentiles = (percentiles,)
+        if not isinstance(percentiles, torch.Tensor):
+            percentiles = torch.tensor(percentiles, dtype=torch.double)
+        if not isinstance(percentiles, torch.Tensor):
+            percentiles = torch.tensor(percentiles)
+        input = input.double()
+        percentiles = percentiles.to(input.device).double()
+        input = input.view(input.shape[0], -1)
+        in_sorted, in_argsort = torch.sort(input, dim=0)
+        positions = percentiles * (input.shape[0]-1) / 100
+        floored = torch.floor(positions)
+        ceiled = floored + 1
+        ceiled[ceiled > input.shape[0] - 1] = input.shape[0] - 1
+        weight_ceiled = positions-floored
+        weight_floored = 1.0 - weight_ceiled
+        d0 = in_sorted[floored.long(), :] * weight_floored[:, None]
+        d1 = in_sorted[ceiled.long(), :] * weight_ceiled[:, None]
+        self.save_for_backward(input_shape, in_argsort, floored.long(),
+                               ceiled.long(), weight_floored, weight_ceiled)
+        result = (d0+d1).view(-1, *input_shape[1:])
+        return result.type(input_dtype)
+
+
+    def backward(self, grad_output):
+        """
+        backward the gradient is basically a lookup table, but with weights
+        depending on the distance between each point and the closest
+        percentiles
+        """
+        (input_shape, in_argsort, floored, ceiled,
+         weight_floored, weight_ceiled) = self.saved_tensors
+
+        # the argsort in the flattened in vector
+
+        cols_offsets = (
+            torch.arange(
+                    0, input_shape[1], device=in_argsort.device)
+            )[None, :].long()
+        in_argsort = (in_argsort*input_shape[1] + cols_offsets).view(-1).long()
+        floored = (
+            floored[:, None]*input_shape[1] + cols_offsets).view(-1).long()
+        ceiled = (
+            ceiled[:, None]*input_shape[1] + cols_offsets).view(-1).long()
+
+        grad_input = torch.zeros((in_argsort.size()), device=self.device)
+        grad_input[in_argsort[floored]] += (grad_output
+                                            * weight_floored[:, None]).view(-1)
+        grad_input[in_argsort[ceiled]] += (grad_output
+                                           * weight_ceiled[:, None]).view(-1)
+
+        grad_input = grad_input.view(*input_shape)
+        return grad_input
+
+    def construct_adjacent_matrix(pc, sparsity):
+        p = Percentile()
+        thresholded = (pc > p(pc, 100 - sparsity))
+        _i = thresholded.nonzero(as_tuple=False).to(args.device)
+        _v = torch.ones(len(_i)).to(args.device)
+        _i = _i.T
+        return torch.sparse.FloatTensor(_i, _v, (pc.shape[0], pc.shape[1]))
+
+    def batch_adjacent_matrix(X, sparsity, self_loop=True):
+        # self_loop=False: 是否在计算阈值时包含自环连接
+        result = []
+        for x in X:
+            if self_loop:
+                a = Percentile.construct_adjacent_matrix(x, sparsity)
+            else:
+                a = Percentile.construct_adjacent_matrix(x - torch.eye(x.shape[0], x.shape[1]), sparsity).to_dense() + torch.eye(
+                    x.shape[0], x.shape[1])
+            result.append(a)
+        return torch.stack(result, dim=0)
